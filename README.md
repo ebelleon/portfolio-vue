836a3f2876f0c9291ddd61108cf78146f3d6b4ef
Leon Ebel <2-ebelleon@users.noreply.gitlab.ebelleon.de>
2020-02-14 15:24:45 +0100

    add dockerize nuxt setup

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# portfolio-vue
+# Portfolio of Leon Ebel
+
+This is the Portfolio of Leon Ebel written in Vue.js with Docker.
+
+![logo](src/assets/img/favicon/favicon-128.png)
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+
+# Build Vue production environment with docker-compose
+docker-compose up -d --build
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+
+---
 
 Copyright &copy; 2020 - Leon Ebel
---
+++ src/stylelint.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {
+    "color-no-invalid-hex": true,
+    "string-no-newline": true,
+    "block-no-empty": true,
+    "comment-no-empty": true,
+    "declaration-block-no-redundant-longhand-properties": true,
+    "declaration-block-single-line-max-declarations": 1,
+    "declaration-colon-space-after": "always-single-line",
+    "declaration-block-semicolon-space-after": "always-single-line",
+    "font-weight-notation": "numeric",
+    "font-family-name-quotes": "always-unless-keyword",
+    "function-comma-space-after": "always",
+    "function-name-case": "lower",
+    "string-quotes": "single",
+  }
+}
