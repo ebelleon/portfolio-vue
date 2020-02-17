4772fc29f8b5eafcc0f54789844ebd847c128e67
Leon Ebel <2-ebelleon@users.noreply.gitlab.ebelleon.de>
2020-02-17 20:23:24 +0100

    add meta tags and imprint page

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -4,17 +4,47 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Leon Ebel - Full Stack Developer - Portfolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content:
+          'Portfolio of Leon Ebel - Front-/ Backend Developer based in Munich, Germany.'
+      },
+      {
+        name: 'keywords',
+        content:
+          'Leon Ebel, Leon, Ebel, developer, development, entwickler, web developer, web development, Portfolio, portfolio, München, Munich, Germany, Deutschland'
+      },
+      { name: 'language', content: 'DE' },
+      { name: 'theme-color', content: '#FF7860' },
+      { name: 'application-name', content: 'www.ebelleon.de' },
+      { name: 'apple-mobile-web-app-title', content: 'www.ebelleon.de' },
+      { name: 'apple-mobile-app-status-bar-style', content: '#FF7860' },
+      {
+        property: 'og:title',
+        content: 'Leon Ebel - Full Stack Developer - Portfolio'
+      },
+      { property: 'og:url', content: 'https://www.ebelleon.de' },
+      { property: 'og:type', content: 'website' },
+      {
+        property: 'og:description',
+        content:
+          'This is the Portfolio of Leon Ebel. Currently working in Munich as a Full-Stack Web Developer.'
       }
+      // { property: 'og:image', content: 'https://www.ebelleon.de/assets/img/background.jpg'} TODO: UNCOMMENT THIS FOR PRODUCTION
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto+Slab:300,700&display=swap'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
