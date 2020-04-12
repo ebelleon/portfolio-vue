2742d1d426c0f2b12fd90f1e189ac450265a4c50
Leon Ebel <2-ebelleon@users.noreply.gitlab.ebelleon.de>
2020-04-12 02:17:51 +0200

    add 2 space indentation to stylelint

--- src/stylelint.config.js
+++ src/stylelint.config.js
@@ -2,18 +2,19 @@ module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
-    'color-no-invalid-hex': true,
-    'string-no-newline': true,
     'block-no-empty': true,
+    'color-no-invalid-hex': true,
     'comment-no-empty': true,
     'declaration-block-no-redundant-longhand-properties': true,
+    'declaration-block-semicolon-space-after': 'always-single-line',
     'declaration-block-single-line-max-declarations': 1,
     'declaration-colon-space-after': 'always-single-line',
-    'declaration-block-semicolon-space-after': 'always-single-line',
-    'font-weight-notation': 'numeric',
     'font-family-name-quotes': 'always-unless-keyword',
+    'font-weight-notation': 'numeric',
     'function-comma-space-after': 'always',
     'function-name-case': 'lower',
+    indentation: 2,
+    'string-no-newline': true,
     'string-quotes': 'single'
   }
 }
