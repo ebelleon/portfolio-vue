8165c21cf297738fcb067fa634da93bfa616c679
Leon Ebel <2-ebelleon@users.noreply.gitlab.ebelleon.de>
2020-04-19 14:14:06 +0200

    add Footer, added into home a commit before

---
+++ src/components/Footer.vue
@@ -0,0 +1,107 @@
+<template>
+  <footer>
+    <div class="socialMedia">
+      <a
+        v-for="(item, index) in linksSocialMedia"
+        :key="index"
+        :href="item.href"
+        :title="item.title"
+        :aria-label="item.label"
+        target="_blank"
+        rel="noopener nofollow"
+      >
+        <fa :icon="['fab', item.icon]"></fa>
+      </a>
+    </div>
+
+    <div class="copyright">
+      {{ /* eslint-disable vue-i18n/no-raw-text */ }}
+      &copy; 2018 - <span>{{ currentYear }}</span> -
+      {{ $t('shared.fullName') }}
+      {{ /* eslint-disable vue-i18n/no-raw-text */ }}
+    </div>
+
+    <div class="disclaimer">
+      <nuxt-link
+        v-for="(item, index) in linksDisclaimer"
+        :key="index"
+        target="_self"
+        :to="item.href"
+        >{{ item.i18n }}</nuxt-link
+      >
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: null,
+      linksSocialMedia: [
+        {
+          href: 'https://www.linkedin.com/in/ebelleon',
+          title: 'LinkedIn',
+          label: 'Personal LinkedIn',
+          icon: 'linkedin'
+        },
+        {
+          href: 'https://www.xing.com/profile/Leon_Ebel',
+          title: 'Xing',
+          label: 'Personal Xing',
+          icon: 'xing'
+        },
+        {
+          href: 'https://www.github.com/ebelleon',
+          title: 'Github',
+          label: 'Personal Github',
+          icon: 'github'
+        }
+      ],
+      linksDisclaimer: [
+        { href: this.localePath('privacy'), i18n: this.$t('title.privacy') },
+        { href: this.localePath('imprint'), i18n: this.$t('title.imprint') }
+      ]
+    }
+  },
+  created() {
+    this.currentYear = new Date().getFullYear()
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'assets/style/partials/vars';
+@import 'assets/style/modules/media-queries';
+
+footer {
+  background-color: $footer;
+  bottom: 0;
+  box-shadow: inset 0 1px 0 0 #4f4a4a, inset 0 2px 4px 0 $codGray;
+  position: fixed;
+  width: 100%;
+  text-align: center;
+  padding: 10px 0;
+}
+
+.copyright,
+.socialMedia,
+.disclaimer {
+  padding: 0.32em 0;
+
+  a {
+    color: $codGray;
+    text-decoration: none;
+    padding: 0 0.77em;
+  }
+
+  @include tablet {
+    display: inline-block;
+    width: 33%;
+
+    a {
+      padding: 0 0.385em;
+    }
+  }
+}
+</style>
