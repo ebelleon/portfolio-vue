866a139395711c12689043ead4fe0855e34d2f33
Leon Ebel <2-ebelleon@users.noreply.gitlab.ebelleon.de>
2020-05-02 19:58:29 +0200

    add BEM, introduce v-for to skills

--- src/pages/skills.vue
+++ src/pages/skills.vue
@@ -10,108 +10,102 @@
       </h2>
 
       <div class="employer-wrapper__content">
-        <a href="https://www.jochen-schweizer.de" target="_blank" rel="noopener nofollow" class="employer-wrapper__link">
-          <svg class="employer-wrapper__link--picture">
-            <use xlink:href="../assets/img/logos.svg#jochen-schweizer-without-slogan" />
-          </svg>
-        </a>
-        {{ /* eslint-disable-next-line */ }}
-        <p>2016</p>
-        <p>{{ $t('aboutMe.developer') }}</p>
-        <p>{{ $t('skills.jochenschweizer') }}</p>
+        <ul>
+          <li>
+            <a href="https://www.jochen-schweizer.de" target="_blank" rel="noopener nofollow" class="employer-wrapper__link">
+              <svg class="employer-wrapper__link--jochenschweizer">
+                <use xlink:href="../assets/img/logos.svg#jochen-schweizer-without-slogan" />
+              </svg>
+            </a>
+            {{ /* eslint-disable-next-line */ }}
+            <p>2016</p>
+            <p>{{ $t('aboutMe.developer') }}</p>
+            <p>{{ $t('skills.jochenschweizer') }}</p>
+          </li>
+
+          <li>
+            <a href="https://www.myposter.de" target="_blank" rel="noopener nofollow" class="employer-wrapper__link">
+              <svg class="employer-wrapper__link--myposter">
+                <use xlink:href="../assets/img/logos.svg#myposter" />
+              </svg>
+            </a>
+            {{ /* eslint-disable-next-line */ }}
+            <p>2020</p>
+            <p>{{ $t('aboutMe.developer') }}</p>
+            <p>{{ $t('skills.myposter') }}</p>
+          </li>
+        </ul>
       </div>
     </div>
 
-    <div class="skillsWrapper">
-      <h2 class="skillsTitle">
+    <div class="skills-wrapper">
+      <h2 class="skills-wrapper__title">
         {{ $t('skills.primary' ) }}
       </h2>
       <br>
-      <div class="skillsPicture DockerPicture" title="Docker">
-        <h4 class="skillsLabel">
-          {{ $t('skills.docker' ) }}
+      <div
+        v-for="(item, index) in primarySkills"
+        :key="index"
+        class="skills-wrapper__picture"
+        :class="`skills-wrapper__picture--${$t(item.label)}`"
+        :title="$t(item.label)"
+      >
+        <h4 class="skills-wrapper__label">
+          {{ $t(item.label) }}
         </h4>
       </div>
-      <div class="skillsPicture PHPPicture" title="PHP">
-        <h4 class="skillsLabel">
-          {{ $t('skills.php' ) }}
-        </h4>
-      </div>
-      <div class="skillsPicture HTMLPicture" title="HTML">
-        <h4 class="skillsLabel">
-          {{ $t('skills.html' ) }}
-        </h4>
-      </div>
-      <input id="CSSDropdownBtn" class="CSSDropdownBtn" type="checkbox">
-      <label for="CSSDropdownBtn">
-        <div class="skillsPicture CSSPicture" title="CSS">
-          <h4 class="skillsLabel">{{ $t('skills.css' ) }} <fa :icon="['fas', 'angle-up']" /></h4>
+      <input id="skills-wrapper__dropdown-btn--CSS" class="skills-wrapper__dropdown-btn--CSS" type="checkbox">
+      <label for="skills-wrapper__dropdown-btn--CSS">
+        <div class="skills-wrapper__picture skills-wrapper__picture--CSS" title="CSS">
+          <h4 class="skills-wrapper__label">{{ $t('skills.css' ) }} <fa :icon="['fas', 'angle-up']" /></h4>
         </div>
       </label>
-      <input id="JavaScriptDropdownBtn" class="JavaScriptDropdownBtn" type="checkbox">
-      <label for="JavaScriptDropdownBtn">
-        <div class="skillsPicture JavaScriptPicture" title="JavaScript">
-          <h4 class="skillsLabel">{{ $t('skills.javascript' ) }} <fa :icon="['fas', 'angle-up']" /></h4>
+      <input id="skills-wrapper__dropdown-btn--JavaScript" class="skills-wrapper__dropdown-btn--JavaScript" type="checkbox">
+      <label for="skills-wrapper__dropdown-btn--JavaScript">
+        <div class="skills-wrapper__picture skills-wrapper__picture--JavaScript" title="JavaScript">
+          <h4 class="skills-wrapper__label">{{ $t('skills.javascript' ) }} <fa :icon="['fas', 'angle-up']" /></h4>
         </div>
       </label>
 
       <!-- Dropdown Content -->
-      <div class="CSSDropdownContent">
-        <div class="skillsPicture LESSPicture" title="LESS">
-          <h4 class="skillsLabel">
-            {{ $t('skills.less' ) }}
-          </h4>
-        </div>
-        <div class="skillsPicture SASSPicture" title="SASS">
-          <h4 class="skillsLabel">
-            {{ $t('skills.sass' ) }}
-          </h4>
-        </div>
-        <div class="skillsPicture BootstrapPicture" title="Bootstrap">
-          <h4 class="skillsLabel">
-            {{ $t('skills.bootstrap' ) }}
+      <div class="skills-wrapper__dropdown-content--CSS">
+        <div
+          v-for="(item, index) in cssContent"
+          :key="index"
+          class="skills-wrapper__picture"
+          :class="`skills-wrapper__picture--${$t(item.label)}`"
+          :title="$t(item.label)"
+        >
+          <h4 class="skills-wrapper__label">
+            {{ $t(item.label) }}
           </h4>
         </div>
       </div>
-      <div class="JavaScriptDropdownContent">
-        <div class="skillsPicture NodejsPicture" title="Node.js">
-          <h4 class="skillsLabel">
+      <div class="skills-wrapper__dropdown-content--JavaScript">
+        <div class="skills-wrapper__picture skills-wrapper__picture--Nodejs" title="Node.js">
+          <h4 class="skills-wrapper__label">
             {{ $t('skills.nodejs' ) }}
           </h4>
         </div>
-        <div class="skillsPicture jQueryPicture" title="jQuery">
-          <h4 class="skillsLabel">
+        <div class="skills-wrapper__picture skills-wrapper__picture--jQuery" title="jQuery">
+          <h4 class="skills-wrapper__label">
             {{ $t('skills.jquery' ) }}
           </h4>
         </div>
       </div>
 
-      <h2 class="skillsTitle">
+      <h2 class="skills-wrapper__title">
         {{ $t('skills.other' ) }}
       </h2>
-      <div class="skillsPicture ScrumPicture" title="Scrum">
-        <h4 class="skillsLabel">
-          {{ $t('skills.scrum' ) }}
-        </h4>
-      </div>
-      <div class="skillsPicture AgilePicture" title="Agile Softwaredevelopment">
-        <h4 class="skillsLabel">
-          {{ $t('skills.agile' ) }}
-        </h4>
-      </div>
-      <div class="skillsPicture SQLPicture" title="SQL">
-        <h4 class="skillsLabel">
-          {{ $t('skills.sql' ) }}
-        </h4>
-      </div>
-      <div class="skillsPicture PerlPicture" title="Perl">
-        <h4 class="skillsLabel">
-          {{ $t('skills.perl' ) }}
-        </h4>
-      </div>
-      <div class="skillsPicture TuxPicture" title="Linux">
-        <h4 class="skillsLabel">
-          {{ $t('skills.linux' ) }}
+      <div
+        v-for="(item, index) in otherSkill"
+        :key="index"
+        class="skills-wrapper__picture"
+        :class="`skills-wrapper__picture--${$t(item.label)}`"
+        :title="$t(item.label)"
+      >
+        <h4 class="skills-wrapper__label">
+          {{ $t(item.label) }}
         </h4>
       </div>
     </div>
@@ -120,6 +114,27 @@
 
 <script>
 export default {
+  data () {
+    return {
+      primarySkills: [
+        { label: 'skills.docker' },
+        { label: 'skills.php' },
+        { label: 'skills.html' }
+      ],
+      cssContent: [
+        { label: 'skills.less' },
+        { label: 'skills.sass' },
+        { label: 'skills.bootstrap' }
+      ],
+      otherSkill: [
+        { label: 'skills.scrum' },
+        { label: 'skills.agile' },
+        { label: 'skills.sql' },
+        { label: 'skills.perl' },
+        { label: 'skills.linux' }
+      ]
+    }
+  },
   nuxtI18n: {
     paths: {
       en: '/skills',
@@ -150,17 +165,24 @@ export default {
     }
   }
 
-  .employer-wrapper__content {
+  &__content {
     display: flex;
     flex-direction: column;
-    justify-content: center;
     margin: auto;
     width: 250px;
     height: 150px;
 
+    ul {
+      list-style-type: none;
+
+      li {
+        margin-bottom: 3rem;
+      }
+    }
+
     p {
       line-height: 2;
-      margin: 0 0 0 7.5rem;
+      margin: 0 0 0 8rem;
       font-size: 0.8rem;
     }
 
@@ -176,24 +198,29 @@ export default {
       margin-left: 5rem;
     }
   }
+  &__link {
+    background-position-y: 55%;
+    border: 2px solid $codGray;
+    border-left-style: dashed;
+    border-right-style: dashed;
+    border-radius: 50%;
+    display: flex;
+    position: absolute;
+    justify-content: center;
+    height: 100px;
+    width: 110px;
+
+    &--myposter {
+      width: 90px;
+    }
+
+    &--jochenschweizer {
+      height: 100px;
+    }
+  }
 }
 
-.employer-wrapper__link {
-  background-position-y: 55%;
-  border: 2px solid $codGray;
-  border-left-style: dashed;
-  border-right-style: dashed;
-  border-radius: 50%;
-  position: absolute;
-  height: 100px;
-}
-
-.employer-wrapper__link--picture {
-  height: 100px;
-  width: 100px;
-}
-
-.skillsWrapper {
+.skills-wrapper {
   overflow: hidden;
   padding: 10em 0 0;
   text-align: center;
@@ -208,19 +235,19 @@ export default {
   }
 }
 
-.employer-wrapper__title, .skillsTitle {
+.employer-wrapper__title, .skills-wrapper__title {
   font-size: $subtitle;
   font-weight: $regular;
   text-align: center;
   text-transform: uppercase;
   margin: 0 2em 0.83em;
 
-  & ~ .skillsTitle {
+  & ~ .skills-wrapper__title {
     margin-top: 6em;
   }
 }
 
-.skillsPicture {
+.skills-wrapper__picture {
   display: inline-block;
   margin: 0.6em 1em 6em;
   height: 85px;
@@ -231,7 +258,7 @@ export default {
   }
 }
 
-.skillsLabel {
+.skills-wrapper__label {
   font-size: 0.90625em;
   font-weight: $light;
   letter-spacing: 0.5px;
@@ -245,7 +272,7 @@ export default {
 
 @each $name in $skillsPictures {
   $i: index($skillsPictures, $name);
-  .#{$name}Picture {
+  .skills-wrapper__picture--#{$name} {
     @include pic(
       $bg-img: url('../assets/img/skills/#{$name}.svg'),
       $bg-size: nth($skillsBgSize, $i)
@@ -254,16 +281,16 @@ export default {
       width: 115px;
     }
     @if $name == 'PHP' {
-      .skillsLabel {
+      .skills-wrapper__label {
         margin-top: 7.5em;
       }
     }
   }
   @if $name == 'CSS' or $name == 'JavaScript' {
-    .#{$name}DropdownBtn {
-      @include dropdownBtn(#{$name}DropdownContent);
+    .skills-wrapper__dropdown-btn--#{$name} {
+      @include dropdownBtn(skills-wrapper__dropdown-content--#{$name});
     }
-    .#{$name}DropdownContent {
+    .skills-wrapper__dropdown-content--#{$name} {
       background-color: $transparent;
       @include dropdownDefault;
 
